174ab83d19e4080265e1b9e88c64024314925c28
Muhammad Badrudinov <mukhammadnur6685@gmail.com>
2026-03-10 10:38:57 +0300

    Add Subcategory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_03_201631) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_10_073703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_03_201631) do
   end
 
   create_table "roles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subcategories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
