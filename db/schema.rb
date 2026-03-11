77862b6c92a53ed9b741440507a7d0d723ef4fe4
Muhammad Badrudinov <mukhammadnur6685@gmail.com>
2026-03-11 12:33:32 +0300

    Create Product_tag and Tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_10_081952) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_11_092729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -83,6 +83,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_081952) do
     t.index ["product_id"], name: "index_product_files_on_product_id"
   end
 
+  create_table "product_tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "product_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_product_tags_on_product_id"
+    t.index ["tag_id"], name: "index_product_tags_on_tag_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -105,6 +114,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_081952) do
   end
 
   create_table "subcategories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
@@ -164,6 +179,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_081952) do
   add_foreign_key "list_tags", "lists"
   add_foreign_key "lists", "products"
   add_foreign_key "product_files", "products"
+  add_foreign_key "product_tags", "products"
+  add_foreign_key "product_tags", "tags"
   add_foreign_key "products", "users"
   add_foreign_key "texts", "products"
   add_foreign_key "user_roles", "roles"
