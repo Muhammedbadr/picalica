f8a87a8e56538702269233a4764bf725ed87cd1f
Muhammad Badrudinov <mukhammadnur6685@gmail.com>
2026-04-21 18:19:00 +0300

    make UI design for Investment & Final Push

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,7 +3,6 @@ import RailsNestedForm from '@stimulus-components/rails-nested-form'
 
 const application = Application.start()
 application.register('nested-form', RailsNestedForm)
-
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
